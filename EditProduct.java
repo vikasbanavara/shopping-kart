2dc4df7f2db3779effb915e25b88df9dc4be5b7e
ftvikas <vikas@fintuple.com>
2022-05-18 15:41:57 +0530

    Edit product code

---
+++ EditProduct.java
@@ -0,0 +1,12 @@
+class EditProduct {
+
+	void edit() {
+
+		System.out.println("Sum is "+a);
+		System.out.println("Thank you for executing code");
+		System.out.println("Please write code");
+		System.out.println("All the best");
+
+	}
+
+}

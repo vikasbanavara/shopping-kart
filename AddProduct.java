b4093bcccbe522d418274f8b67d1e2bad27dcf5f
ftvikas <vikas@fintuple.com>
2022-05-18 15:34:29 +0530

    Code to add products

---
+++ AddProduct.java
@@ -0,0 +1,17 @@
+class AddProduct {
+
+	void add() {
+
+		int a = 10 + 20;
+		int c = 2 + 90;
+		int d = 15 + 22;
+		double pi = 3.142;
+
+		System.out.println("Sum is "+a);
+		System.out.println("Thank you for executing code");
+		System.out.println("Please write code");
+		System.out.println("All the best");
+
+	}
+
+}

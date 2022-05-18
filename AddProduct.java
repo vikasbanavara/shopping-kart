54cf72fded0b4e5c14bedd6522a224e880ed4613
ftvikas <vikas@fintuple.com>
2022-05-18 15:48:48 +0530

    Add product modified code

--- AddProduct.java
+++ AddProduct.java
@@ -6,10 +6,12 @@ class AddProduct {
 		int c = 2 + 90;
 		int d = 15 + 22;
 		double pi = 3.142;
-
+		//Adding line A1 by Ramu
+		//Adding line A2 by Ramu
 		System.out.println("Sum is "+a);
 		System.out.println("Thank you for executing code");
 		System.out.println("Please write code");
+		//Adding line A3 by Ramu
 		System.out.println("All the best");
 
 	}
